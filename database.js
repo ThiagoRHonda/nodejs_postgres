bf8d610710dd31f6ac05e1f5f70b294cc212922a
ThiagoRHonda <trossettohonda@gmail.com>
2023-09-20 14:41:37 -0300

    Commit Inicial

---
+++ database.js
@@ -0,0 +1,60 @@
+async function connect() {
+
+    if(global.connection)
+        return global.connection.connect();
+
+    const { Pool } = require("pg");
+    const pool = new Pool({
+        connectionString: process.env.CONNECTION_STRING
+    });
+
+    const client = await pool.connect();
+    console.log("Criou o pool de conexão");
+
+    const res = await client.query("select now()");
+    console.log(res.rows[0]);
+    client.release();
+
+    global.connection = pool;
+    return pool.connect();
+}
+
+connect();
+
+async function selectCustomer(id) {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM clientes WHERE ID=$1", [id]);
+    return res.rows;
+}
+
+async function selectCustomers() {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM clientes");
+    return res.rows;
+}
+
+async function insertCustomer(customer) {
+    const client = await connect();
+    const sql = "INSERT INTO clientes(nome,idade,uf_id) VALUES($1, $2, $3)"
+    await client.query(sql, [customer.nome, customer.idade, customer.uf_id]);
+}
+
+async function updateCustomer(id, customer) {
+    const client = await connect();
+    const sql = "UPDATE clientes SET nome=$1, idade=$2, uf_id=$3 WHERE id=$4"
+    await client.query(sql, [customer.nome, customer.idade, customer.uf_id, id]);
+}
+
+async function deleteCustomer(id) {
+    const client = await connect();
+    const sql = "DELETE FROM clientes WHERE id=$1"
+    await client.query(sql, [id]);
+}
+
+module.exports = {
+    selectCustomers,
+    selectCustomer,
+    insertCustomer,
+    updateCustomer,
+    deleteCustomer
+}
---
+++ index.js
@@ -0,0 +1,49 @@
+//carrega as variaveis do .env
+require("dotenv").config();
+
+const { database } = require("pg/lib/defaults");
+const db = require("./database");
+
+const port = process.env.PORT;
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Funcionando!"
+    })
+})
+
+app.get("/clientes/:id", async (req, res) => {
+    const cliente = await db.selectCustomer(req.params.id);
+    res.json(cliente);
+})
+
+
+app.get("/clientes", async (req, res) => {
+    const clientes = await db.selectCustomers();
+    res.json(clientes);
+})
+
+app.post("/clientes", async (req, res) => {
+    await db.insertCustomer(req.body);
+    res.sendStatus(201);
+})
+
+app.patch("/clientes/:id", async (req, res) => {
+    await db.updateCustomer(req.params.id, req.body);
+    res.sendStatus(200);
+})
+
+app.delete("/clientes/:id", async (req, res) => {
+    await db.deleteCustomer(req.params.id);
+    res.sendStatus(204);
+})
+
+app.listen(port);
+
+console.log("Backend rodando!");
